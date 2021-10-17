ad65d970531967686cd9f1cb869c1b99e55ab69e
bedimcode <61248995+bedimcode@users.noreply.github.com>
2021-10-17 10:47:17 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+/*=============== SHOW MODAL ===============*/
+const showModal = (openButton, modalContent) =>{
+    const openBtn = document.getElementById(openButton),
+    modalContainer = document.getElementById(modalContent)
+    
+    if(openBtn && modalContainer){
+        openBtn.addEventListener('click', ()=>{
+            modalContainer.classList.add('show-modal')
+        })
+    }
+}
+showModal('open-modal','modal-container')
+
+/*=============== CLOSE MODAL ===============*/
+const closeBtn = document.querySelectorAll('.close-modal')
+
+function closeModal(){
+    const modalContainer = document.getElementById('modal-container')
+    modalContainer.classList.remove('show-modal')
+}
+closeBtn.forEach(c => c.addEventListener('click', closeModal))
